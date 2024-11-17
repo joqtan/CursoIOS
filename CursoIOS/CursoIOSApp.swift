0ba2993d05380a209c79ddf2c1271a36ffad7a0b
Joqtán Mármol <joqtanmarmol@gmail.com>
2024-11-17 13:24:58 -0600

    Initial Commit

---
+++ CursoIOS/CursoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoIOSApp.swift
+//  CursoIOS
+//
+//  Created by Joqtan on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CursoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
